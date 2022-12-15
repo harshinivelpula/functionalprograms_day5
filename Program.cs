f742b9026fec77499f90f7400762a3e9415c67f5
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-15 14:25:06 +0530

    all programs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace logicsforcoreprograms5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            logics.CheckingVowelOrConsonant();
+            logics.CheckingQuotientAndReminder();
+            logics.Factor();
+            logics.Check_Even_Or_Odd();
+            logics.Leap_Year();
+            logics.Largest_Among_Three_Numbers();
+            logics.Swap_Two_Number();
+            logics.FlipCoinMethod();
+            logics.Harmonic();
+            logics.Power_Of_2();
+        }
+    }
+}
